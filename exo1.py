3d9e75ff205802450b4ab8263907b87a704543fc
Raneksa <hei.nekena.34@gmail.com>
2025-09-01 15:04:41 +0300

    modified

--- exo1.py
+++ exo1.py
@@ -1,8 +1,5 @@
-from typing import List
 
 from fastapi import FastAPI
-from pydantic import BaseModel
-from starlette.requests import Request
 from starlette.responses import Response, JSONResponse
 
 app = FastAPI();
