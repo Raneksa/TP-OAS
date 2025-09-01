1c57efec1d9df8d59eccd2e1368d9072b016b9b3
Raneksa <hei.nekena.34@gmail.com>
2025-09-01 15:02:03 +0300

    first commit

---
+++ exo1.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import Response, JSONResponse
+
+app = FastAPI();
+
+@app.Get("/ping")
+def pong():
+    return Response(content="pong", status_code=200, media_type="text/plain")
