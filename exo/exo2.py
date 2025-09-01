01b4f7a2f8f3696426bab344262d356c8b17a0e0
Raneksa <hei.nekena.34@gmail.com>
2025-09-01 15:46:24 +0300

    achieved

--- exo/exo2.py
+++ exo/exo2.py
@@ -1,2 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import Response, JSONResponse;
+
+
+app = FastAPI()
+
+@app.get("/users/{user_id}page={page}&size={size}")
+def get_user(user_id: int, page: int = 1, size: int = 20):
+    user_data = {
+        "id": user_id,
+        "page": page,
+        "size": size,
+        }
+    return JSONResponse(content=user_data, status_code=200);
+    return JSONResponse(content="Bad type for provided querry parameters", status_code=404);
+
